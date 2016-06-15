53995f85515493dcb176ce559b098bea877501a7
Gijs Wilbrink <gijs@occhio.nl>
2016-06-15 10:00:38 +0200

    Setup en eerste actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,33 @@
+// increment
+function increment(index) {
+	return {
+		type 'INCREMENT_LIKES',
+		index
+	}
+}
+
+// add comment
+function addComment(postId, author, comment) {
+	return {
+		type 'ADD_COMMENT',
+		postId, 
+		author, 
+		comment
+	}
+}
+
+// remove comment
+function removeComment(postId, i) {
+	return {
+		type 'REMOVE_COMMENT',
+		postId, 
+		i
+	}
+}
+
+/**
+ * Export
+ */
+export increment;
+export addComment;
+export removeComment;
---
+++ client/store.js
@@ -0,0 +1,23 @@
+/**
+ * Import framework
+ */
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create default state
+const defaultState = {
+	posts,
+	comments
+};
+
+const store = createStore(rootReducer, defaultState);
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
