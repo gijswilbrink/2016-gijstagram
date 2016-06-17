388a4c384f78b31c9c7bc6125a6ee7c57d9f2c17
Gijs Wilbrink <gijs@occhio.nl>
2016-06-17 14:47:22 +0200

    Redux devTools

--- client/store.js
+++ client/store.js
@@ -1,7 +1,7 @@
 /**
  * Import framework
  */
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -17,9 +17,12 @@ const defaultState = {
 	comments
 };
 
-const store = createStore(rootReducer, defaultState, 
-	window.devToolsExtension && window.devToolsExtension()
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
 );
+
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
